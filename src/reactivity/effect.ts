28a3d8de6bb827cb211757051c14ae1f24d94ade
yuanyanmin <1414011709@qq.com>
2022-07-27 23:04:18 +0800

    修改effect逻辑

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,14 +1,46 @@
-class ReactiveEffect {
-  private _fn:any;
+import { createDep } from "./dep";
+export const extend = Object.assign;
+
+let shouldTrack = false
+let activeEffect = void 0
+const targetMap = new WeakMap();
+
+export class ReactiveEffect {
   deps = []
   active = true;
   onStop?: () => void;
-  constructor (fn, scheuler) {
-    this._fn = fn;
+  constructor (public fn, public scheuler?) {
+    console.log("创建 ReactiveEffect 对象");
+
   }
   run() {
-    activeEffect = this;
-    return this._fn()
+    console.log("run");
+    // 运行 run 的时候，可以控制 要不要执行后续收集依赖的一步
+    // 目前来看的话，只要执行了 fn 那么就默认执行了收集依赖
+    // 这里就需要控制了
+
+    // 是不是收集依赖的变量
+
+    // 执行 fn  但是不收集依赖
+    if (!this.active) {
+      return this.fn();
+    }
+
+    // 执行 fn  收集依赖
+    // 可以开始收集依赖了
+    shouldTrack = true;
+
+    // 执行的时候给全局的 activeEffect 赋值
+    // 利用全局属性来获取当前的 effect
+    activeEffect = this as any;
+    // 执行用户传入的 fn
+    console.log("执行用户传入的 fn");
+    const result = this.fn();
+    // 重置
+    shouldTrack = false;
+    activeEffect = undefined;
+
+    return result;
   }
 
   stop() {
@@ -27,59 +59,113 @@ function clearupEffect(effect:any) {
   effect.deps.forEach((dep:any) => {
     dep.delete(effect)
   });
+  effect.deps.length = 0;
+}
+
+export function effect(fn, options = {}) {
+  const _effect = new ReactiveEffect(fn);
+
+  // 把用户传过来的值合并到 _effect 对象上去
+  // 缺点就是不是显式的，看代码的时候并不知道有什么值
+  extend(_effect, options);
+  _effect.run();
+
+  // 把 _effect.run 这个方法返回
+  // 让用户可以自行选择调用的时机（调用 fn）
+  const runner: any = _effect.run.bind(_effect);
+  runner.effect = _effect;
+  return runner;
+}
+
+export function stop(runner) {
+  runner.effect.stop();
 }
 
 // 收集依赖
-const targetMap = new Map()
 export function track(target, type, key) {
+  // 先收集所有的 dep 放到 deps 里面
   // target -> key -> dep
+  if (!isTracking()) {
+    return;
+  }
+
   let depsMap = targetMap.get(target)
 
+  // 1. 先基于 target 找到对应的 dep
+  // 如果是第一次的话，那么就需要初始化
   if (!depsMap) {
+    // 初始化 depsMap 的逻辑
     depsMap = new Map()
     targetMap.set(target, depsMap)
   }
 
-  let dep = depsMap.get(key)
+  let dep = depsMap.get(key);
 
   if (!dep) {
-    dep = new Set()
+    dep = createDep()
+
     depsMap.set(key, dep)
   }
-  if (!activeEffect) return
-  dep.add(activeEffect)
-  activeEffect.deps.push(dep)
 
-
+  trackEffects(dep)
 }
+export function trackEffects(dep) {
+  // 用 dep 来存放所有的 effect
 
-// 触发依赖
-export function trigger(target, type, key) {
-  let depsMap = targetMap.get(target)
-  let dep = depsMap.get(key)
-
-  for(const effect of dep) {
-    if (effect.scheuler) {
-      effect.scheuler()
-    } else {
-      effect.run()
-    }
+  // TODO
+  // 这里是一个优化点
+  // 先看看这个依赖是不是已经收集了，
+  // 已经收集的话，那么就不需要在收集一次了
+  // 可能会影响 code path change 的情况
+  // 需要每次都 cleanupEffect
+  // shouldTrack = !dep.has(activeEffect!);
+  if (!dep.has(activeEffect)) {
+    dep.add(activeEffect);
+    (activeEffect as any).deps.push(dep);
   }
 }
+// 触发依赖
+export function trigger(target, type, key) {
+  // 1. 先收集所有的 dep 放到 deps 里面，
+  // 后面会统一处理
+  let deps: Array<any> = [];
+  // dep
 
-let activeEffect
-export function effect(fn, options:any = {}) {
-  const scheuler = options.scheuler
-  const _effect = new ReactiveEffect(fn, scheuler)
-  // _effect.onStop = options.onStop
-  Object.assign(_effect, options)
+  const depsMap = targetMap.get(target);
 
-  _effect.run()
-  const runner:any = _effect.run.bind(_effect)
-  runner.effect = _effect
-  return runner
+  if (!depsMap) return;
+
+  // 暂时只实现了 GET 类型
+  // get 类型只需要取出来就可以
+  const dep = depsMap.get(key);
+
+  // 最后收集到 deps 内
+  deps.push(dep);
+
+  const effects: Array<any> = [];
+  deps.forEach((dep) => {
+    // 这里解构 dep 得到的是 dep 内部存储的 effect
+    effects.push(...dep);
+  });
+  // 这里的目的是只有一个 dep ，这个dep 里面包含所有的 effect
+  // 这里的目前应该是为了 triggerEffects 这个函数的复用
+  triggerEffects(createDep(effects));
 }
 
-export function stop(runner) {
-  runner.effect.stop()
+export function isTracking() {
+  return shouldTrack && activeEffect !== undefined;
+}
+
+export function triggerEffects(dep) {
+  // 执行收集到的所有的 effect 的 run 方法
+  for (const effect of dep) {
+    if (effect.scheduler) {
+      // scheduler 可以让用户自己选择调用的时机
+      // 这样就可以灵活的控制调用了
+      // 在 runtime-core 中，就是使用了 scheduler 实现了在 next ticker 中调用的逻辑
+      effect.scheduler();
+    } else {
+      effect.run();
+    }
+  }
 }
